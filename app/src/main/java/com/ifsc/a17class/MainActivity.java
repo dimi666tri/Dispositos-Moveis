54d0c943666305bd9bc86a0f129f4db3c3c7bbf5
CAMILA <camila.s11@aluno.ifsc.edu.br>
2025-03-17 09:13:32 -0300

    Conta Click

---
+++ app/src/main/java/com/ifsc/a17class/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ifsc.a17class;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    int i = 0 ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        // associando objeto interface a variavel local
+
+
+
+        TextView tv = findViewById(R.id.textedView);
+        tv.setText(R.string.app_name);
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(v -> {
+            tv.setText(Integer.toString(i));
+            i++;
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
